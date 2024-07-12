7d22469bc53573d3831b6889f19ef0aba62f97b7
João Pedro <joaopedromouramendes1@gmail.com>
2024-07-12 20:45:37 -0300

    feat: update DepartmentService

--- src/main/java/com/store/api/domain/department/DepartmentService.java
+++ src/main/java/com/store/api/domain/department/DepartmentService.java
@@ -1,9 +1,12 @@
 package com.store.api.domain.department;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class DepartmentService {
@@ -17,5 +20,28 @@ public class DepartmentService {
 
     public void createDepartment(PostRequestDepartment data) {
         departmentRepository.save(new Department(data));
+    }
+
+    public Department updateDepartment(UUID id, PutRequestDepartment data) {
+        // Busca o objeto no banco de dados
+        Optional<Department> optionalDepartment = departmentRepository.findById(id);
+        // Valida se o objeto foi encontrado
+        if (optionalDepartment.isPresent()) {
+            Department department = optionalDepartment.get();
+            department.setName(data.name());
+            return department;
+        }
+        return null;
+    }
+
+    public boolean deleteDepartment(UUID id) {
+        // Busca o objeto no banco de dados
+        Optional<Department> optionalDepartment = departmentRepository.findById(id);
+        if (optionalDepartment.isPresent()) {
+            Department department = optionalDepartment.get();
+            departmentRepository.delete(department);
+            return true;
+        }
+        return false;
     }
 }
